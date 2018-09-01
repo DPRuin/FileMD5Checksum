3ae47623c874ece98addc1bc46c24911dc184d6c
mac126 <tulaomao152@126.com>
2018-09-01 15:00:16 +0800

    Create FileChecksum

---
+++ FileMD5Checksum/FileMD5Checksum/FileChecksum.swift
@@ -0,0 +1,14 @@
+//
+//  FileChecksum.swift
+//  FileMD5Checksum
+//
+//  Created by mac126 on 2018/9/1.
+//  Copyright © 2018年 mac126. All rights reserved.
+//
+
+import UIKit
+
+class FileChecksum {
+    
+
+}
