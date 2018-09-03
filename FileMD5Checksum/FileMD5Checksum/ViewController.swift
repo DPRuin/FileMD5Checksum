97e22180cc0b7d667a92b2a41893bdeece28154a
mac126 <tulaomao152@126.com>
2018-09-03 11:18:07 +0800

    checksum

--- FileMD5Checksum/FileMD5Checksum/ViewController.swift
+++ FileMD5Checksum/FileMD5Checksum/ViewController.swift
@@ -19,7 +19,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        let stringmd5 = FileChecksum.genMD5Checksum("hhhhh")
+//        print("\(stringmd5)")
+        // housekeeper-1.0
+        let zipurl = Bundle.main.url(forResource: "idle", withExtension: "zip")
+        let filemd5 = FileChecksum.genMD5Checksum(fromFile: zipurl)
+        
+        if let filemd5 = filemd5 {
+            print("\(filemd5)")
+        }
+        
+    }
 
+    
 
 }
 
